2ad61534436e86f38d465f72ffa32adb28bdcbcd
Alex Megremis <>
2021-03-26 05:31:48 +0000

    WIP Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <div className="jumbotron text-center">
+                    <h1>Codility</h1>
+                </div>
+                <div className="form-inline">
+                    <label>Start your search : </label>
+                    <input type="text"/>
+                    <button type="submit" onClick="">Search</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
